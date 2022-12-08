2746908cf0da899c89a5670ffefecde674746949
doheeMac <thsehgml37@korea.ac.kr>
2022-12-08 14:38:05 +0900

    feat: post action method(decide after single movement)

--- src/UI/OutputView.js
+++ src/UI/OutputView.js
@@ -38,7 +38,9 @@ const OutputView = {
    * <p>
    * 출력을 위해 필요한 메서드의 인자(parameter)는 자유롭게 추가하거나 변경할 수 있다.
    */
-  printResult() {},
+  printResult(gameRoundResult, gameRound) {
+    console.log('Todo: 성공');
+  },
 
   printError(errorType) {
     Console.print(PREFIX + ERROR_MESSAGE[errorType]);
--- src/components/Game.js
+++ src/components/Game.js
@@ -37,13 +37,13 @@ class Game {
   postAction(moveResult) {
     if (!moveResult) {
       // 실패 - 게임 다시 할지 물어보고 다시하면 retry 메서드 작성해주기
-      return InputView.readGameCommand.bind(this)(this.askRetry);
+      return InputView.readGameCommand.bind(this)(this.lostProcess);
     }
 
     const CONTINUE = this.#bridgeGame.gameStatus();
     if (CONTINUE) {
       // 게임 계속
-      this.makeMovement();
+      InputView.readMoving.bind(this)(this.moveProcess);
     }
     if (!CONTINUE) {
       // 게임을 성공적으로 마침!
@@ -51,7 +51,13 @@ class Game {
   }
 
   lostProcess(retryCommand) {
-    this.#errorHandler('RETRY', () => {});
+    this.#errorHandler('RETRY', () => {
+      console.log(retryCommand);
+    });
+  }
+
+  quitGame(gameResult) {
+    // Todo: 게임 결과 출력후 게임 마침 콘솔 닫아주기
   }
 
   #errorHandler(errorType, callback) {

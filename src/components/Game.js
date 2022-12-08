153db352f7a3a5e60a1cebfebfe905de8fc95416
doheeMac <thsehgml37@korea.ac.kr>
2022-12-08 10:36:00 +0900

    feat: game error handler 기능

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 const OutputView = require('../UI/OutputView.js');
 const InputView = require('../UI/InputView.js');
+const BridgeGame = require('../BridgeGame.js');
 
 class Game {
   #bridgeGame;
@@ -8,7 +9,25 @@ class Game {
     InputView.readBridgeSize.bind(this)(this.createBridge);
   }
 
-  createBridge(bridgeSize) {}
+  createBridge(bridgeSize) {
+    this.tryCatch(() => {
+      this.#bridgeGame = new BridgeGame(bridgeSize);
+      //
+    });
+  }
+
+  errorHandler(callback) {
+    try {
+      callback();
+    } catch (error) {
+      this.errorResponse(error);
+    }
+  }
+
+  errorResponse(error) {
+    console.log(error);
+    //OutputView.printError('BRIDGE_SIZE');
+  }
 }
 
 module.exports = Game;

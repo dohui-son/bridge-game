ef35e92de2e4453beb462d0b29dfb36b5a496a0f
doheeMac <thsehgml37@korea.ac.kr>
2022-12-08 20:38:59 +0900

    docs: requirement note updated

--- bridge-game-2/src/BridgeRandomNumberGenerator.js
+++ bridge-game-2/src/BridgeRandomNumberGenerator.js
@@ -1,14 +1,19 @@
-const MissionUtils = require("@woowacourse/mission-utils");
+const MissionUtils = require('@woowacourse/mission-utils');
+
+/*REQUIREMENTS
+Random 값 추출은 제공된 BridgeRandomNumberGenerator의 generate()를 활용한다.
+BridgeRandomNumberGenerator의 코드는 변경할 수 없다.
+*/
 
 const BridgeRandomNumberGenerator = {
-  RANDOM_LOWER_INCLUSIVE: 0,
-  RANDOM_UPPER_INCLUSIVE: 1,
-  generate() {
-    return MissionUtils.Random.pickNumberInRange(
-      BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE,
-      BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE
-    );
-  },
+	RANDOM_LOWER_INCLUSIVE: 0,
+	RANDOM_UPPER_INCLUSIVE: 1,
+	generate() {
+		return MissionUtils.Random.pickNumberInRange(
+			BridgeRandomNumberGenerator.RANDOM_LOWER_INCLUSIVE,
+			BridgeRandomNumberGenerator.RANDOM_UPPER_INCLUSIVE
+		);
+	},
 };
 
 module.exports = BridgeRandomNumberGenerator;
